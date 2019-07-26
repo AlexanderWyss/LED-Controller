04639e5d8acc4651ba1c47738679c011408a5d7f
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-26 14:04:52 +0200

    test page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'patterns',
     pathMatch: 'full'
   },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+  { path: 'patterns', loadChildren: './patterns/patterns.module#PatternsPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/patterns/patterns.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-patterns',
+  templateUrl: './patterns.page.html',
+  styleUrls: ['./patterns.page.scss'],
+})
+export class PatternsPage implements OnInit {
+
+  BASE = '/';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  wave() {
+    this.http.get(this.BASE + 'wave').subscribe();
+  }
+
+  rainbowCycle() {
+    this.http.get(this.BASE + 'rainbow').subscribe();
+  }
+
+  colorRider() {
+    this.http.get(this.BASE + 'rider').subscribe();
+  }
+
+  strobe() {
+    this.http.get(this.BASE + 'strobe').subscribe();
+  }
+
+  runningLight() {
+    this.http.get(this.BASE + 'running').subscribe();
+  }
+
+  rainbowChase() {
+    this.http.get(this.BASE + 'chase').subscribe();
+  }
+
+  rainbowSparkle() {
+    this.http.get(this.BASE + 'sparkle').subscribe()
+  }
+}
