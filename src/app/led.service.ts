71303e5ad08d5e922e93fe32e0c3944492a1875b
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-07-28 16:12:01 +0200

    com selection

--- src/app/led.service.ts
+++ src/app/led.service.ts
@@ -1,11 +1,24 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {Pattern} from "./pattern.service";
+import {Pattern} from './pattern.service';
+import {map} from 'rxjs/operators';
+
+export interface SerialPort {
+    comName: string;
+    manufacturer?: string;
+    serialNumber?: string;
+    pnpId?: string;
+    locationId?: string;
+    productId?: string;
+    vendorId?: string;
+}
 
 @Injectable({
     providedIn: 'root'
 })
 export class LEDService {
+
+    private selectedPort: string;
 
     BASE = '/api/';
 
@@ -26,13 +39,28 @@ export class LEDService {
         this.http.get(this.BASE + 'alloff').subscribe();
     }
 
-  save(pattern: Pattern) {
-    let param = new HttpParams()
-        .set('pattern', pattern.name);
-    for (const patternSetting of pattern.patternSettings) {
-      console.log(patternSetting.value.toString());
-      param = param.set(patternSetting.name, patternSetting.value.toString());
+    public save(pattern: Pattern) {
+        let param = new HttpParams()
+            .set('pattern', pattern.name);
+        for (const patternSetting of pattern.patternSettings) {
+            console.log(patternSetting.value.toString());
+            param = param.set(patternSetting.name, patternSetting.value.toString());
+        }
+        this.http.get(this.BASE + 'options', {params: param}).subscribe();
     }
-    this.http.get(this.BASE + 'options', {params: param}).subscribe();
-  }
+
+    public getSerialports(): Promise<SerialPort[]> {
+        return this.http.get(this.BASE + 'serialport/get').pipe(map((result: any) => result.serialports)).toPromise();
+    }
+
+    public setSerialport(name: string) {
+        this.selectedPort = name;
+        const param = new HttpParams()
+            .set('name', name);
+        this.http.get(this.BASE + 'serialport/set', {params: param}).subscribe();
+    }
+
+    public getSelectedPort() {
+        return this.selectedPort;
+    }
 }
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LEDService, SerialPort} from '../led.service';
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+    selector: 'app-settings',
+    templateUrl: './settings.page.html',
+    styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+    private serialports: SerialPort[];
+    private selectedPort;
 
-  ngOnInit() {
-  }
+    constructor(private ledService: LEDService) {
+    }
 
+    ngOnInit() {
+        this.ledService.getSerialports().then(serialports => this.serialports = serialports);
+        if (this.ledService.getSelectedPort()) {
+            this.selectedPort = this.ledService.getSelectedPort();
+        } else {
+            this.selectedPort = 'autoselect';
+        }
+    }
+
+    setPort() {
+        this.ledService.setSerialport(this.selectedPort);
+    }
 }
