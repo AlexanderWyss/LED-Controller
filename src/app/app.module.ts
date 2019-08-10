977925fea36c01e0024fb9ffb6c8820305c94662
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-08-10 14:55:16 +0200

    init bluetooth impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,16 @@ import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {PatternSettingsSliderComponent} from './pattern-settings-slider/pattern-settings-slider.component';
 import {FormsModule} from '@angular/forms';
 import {PatternSettingsModule} from './pattern-settings/pattern-settings.module';
 import { ColorPickerModule } from '@iplab/ngx-color-picker';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {BLE} from '@ionic-native/ble/ngx';
 
 @NgModule({
     declarations: [AppComponent],
@@ -31,7 +29,9 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
     providers: [
         StatusBar,
         SplashScreen,
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+        BLE
+
     ],
     exports: [],
     bootstrap: [AppComponent]
