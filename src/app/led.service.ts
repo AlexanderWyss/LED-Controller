c0ff037ca12d736e2b441f0b3dc8fb82b67def5e
Alexander Wyss <alexsilvan.wyss@gmail.com>
2019-08-11 21:48:06 +0200

    toasti

--- src/app/led.service.ts
+++ src/app/led.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {ComProviderService} from "./com-provider.service";
 import {ComService} from "./com.service";
 import {Pattern} from "./pattern.service";
+import {ToastService} from "./toast.service";
 
 export interface PortInfo {
   serialports: string[];
@@ -15,19 +16,20 @@ export class LEDService {
 
   BASE = "/api/";
 
-  constructor(private comProvider: ComProviderService) {
+  constructor(private comProvider: ComProviderService, private toast: ToastService) {
   }
 
   public start(name: string) {
-    this.getCom().write(this.BASE + "start", {pattern: name});
+    this.getCom().write(this.BASE + "start", {pattern: name})
+      .catch(this.logError("starting " + name));
   }
 
   public stop() {
-    this.getCom().read(this.BASE + "stop");
+    this.getCom().read(this.BASE + "stop").catch(this.logError("stopping"));
   }
 
   public allOff() {
-    this.getCom().read(this.BASE + "alloff");
+    this.getCom().read(this.BASE + "alloff").catch(this.logError("turning all off"));
   }
 
   public save(pattern: Pattern) {
@@ -36,11 +38,11 @@ export class LEDService {
       console.log(patternSetting.value.toString());
       data[patternSetting.name] = patternSetting.value.toString();
     }
-    this.getCom().write(this.BASE + "options", data);
+    this.getCom().write(this.BASE + "options", data).catch(this.logError("saving " + pattern.name));
   }
 
   public setNumberOfLeds(leds: number) {
-    this.getCom().write(this.BASE + "leds/set", {leds: leds.toString()});
+    this.getCom().write(this.BASE + "leds/set", {leds: leds.toString()}).catch(this.logError("setting Number of Leds"));
   }
 
   public getNumberOfLeds(): Promise<number> {
@@ -48,7 +50,7 @@ export class LEDService {
   }
 
   public setPin(pin: string) {
-    this.getCom().write(this.BASE + "pin/set", {pin});
+    this.getCom().write(this.BASE + "pin/set", {pin}).catch(this.logError("setting pin"));
   }
 
   public getPin(): Promise<string> {
@@ -60,10 +62,17 @@ export class LEDService {
   }
 
   public setSerialport(port: string) {
-    this.getCom().write(this.BASE + "serialport/set", {name: port});
+    this.getCom().write(this.BASE + "serialport/set", {name: port}).catch(this.logError("setting serialport"));
   }
 
   private getCom(): ComService {
     return this.comProvider.getCom();
+  }
+
+  private logError(action: string) {
+    return error => {
+      console.error(error);
+      this.toast.error("Something went wrong while " + action);
+    };
   }
 }
